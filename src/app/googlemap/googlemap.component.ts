f69c8069eb373b241d3569c487923aca80caeb76
edzio94 <edzio94@gmail.com>
2016-07-19 14:30:56 +0200

    first commit

--- src/app/googlemap/googlemap.component.ts
+++ src/app/googlemap/googlemap.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-
+import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
+import {SebmGoogleMap} from 'angular2-google-maps/core';
 @Component({
   selector: 'googleMap',
-  templateUrl: './googlemap.template.html'
+  directives: [SebmGoogleMap],
+  styles: [`
+   .sebm-google-map-container {
+     height: 300px;
+   }
+`],
+  template: `
+   <sebm-google-map [latitude]="lat" [longitude]="lng" [zoom]="zoom">
+   </sebm-google-map>
+ `
 })
 
-export  class Googlemap {
+export class Googlemap {
+  title: string = 'My first angular2-google-maps project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  zoom: number = 10;
+  apikey: 'AIzaSyAH4PpkdHM6nXyfbu6b7HNqnKp201Yq1AI';
   ngOnInit() {
     console.log('hello `googlemap` component');
-
   }
 }
+
+
--- src/app/index.ts
+++ src/app/index.ts
@@ -3,9 +3,11 @@ export * from './app.component';
 export * from './app.service';
 export * from './app.routes';
 
+import {GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 import { AppState } from './app.service';
 
 // Application wide providers
 export const APP_PROVIDERS = [
-  AppState
+  AppState,
+  GOOGLE_MAPS_PROVIDERS
 ];
